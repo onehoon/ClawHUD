7642780e37b8470eab86ac406b1cf52875159557
onehoon <onehoon@gmail.com>
2026-08-31 17:19:45 +0900

    Add standalone diagnostic foundation

---
+++ src/ClawHUD.Diag/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "ClawHUD Game Detection Diagnostic\n"
+              << "Diagnostic capture is not implemented in this foundation build.\n";
+    return 0;
+}
